245122349e5d0d12ef93aad39390083c6d5b98c2
Daniel Alves <daniel.alves@portus.tech>
2023-10-11 00:55:58 -0300

    simplified git-clip command

--- clip/branch.go
+++ clip/branch.go
@@ -1,16 +1,13 @@
 package clip
 
 import (
-	"fmt"
 	"git_extensions/search"
 	"git_extensions/shared/clipboard"
 	"git_extensions/shared/errors"
 	"git_extensions/shared/git"
 	"github.com/spf13/cobra"
-	"os"
 )
 
-var current bool
 var searchValue string
 
 // branchCmd represents the branch command
@@ -19,15 +16,9 @@ var branchCmd = &cobra.Command{
 	Short: "copies current git branch to clipboard",
 	Long:  ``,
 	Run: func(cmd *cobra.Command, args []string) {
-		// sanity check
-		if current && searchValue != "" {
-			fmt.Println("Cannot use both --current and --search")
-			os.Exit(1)
-		}
-
 		// get current branch
 		var branch string
-		if current {
+		if searchValue == "" {
 			var err error
 			branch, err = git.CurrentBranch()
 			errors.HandleError(err)
@@ -40,12 +31,6 @@ var branchCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(branchCmd)
-	branchCmd.Flags().BoolVarP(
-		&current,
-		"current",
-		"c",
-		false,
-		"copies current git branch to clipboard")
 	branchCmd.Flags().StringVarP(
 		&searchValue,
 		"search",
